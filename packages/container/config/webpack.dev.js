e6f01f4f561b69294b4e281a10c989f4989b5cb0
salix <sal.senturk@gmail.com>
2021-05-07 23:00:02 +0300

    randomize production css classnames

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -4,22 +4,22 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
-	mode: 'development',
-	devServer: {
-		port: 8080,
-		historyApiFallback: {
-			index: 'index.html',
-		},
-	},
-	plugins: [
-		new ModuleFederationPlugin({
-			name: 'container',
-			remotes: {
-				marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-			},
-			shared: packageJson.dependencies,
-		}),
-	],
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
 };
 
 module.exports = merge(commonConfig, devConfig);
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
 
-// mount function to start up the app
+// Mount function to start up the app
 const mount = (el) => {
-	ReactDOM.render(<App />, el);
+  ReactDOM.render(<App />, el);
 };
 
-// if we are in dev and in isolation, call mount immediately
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
-	const devRoot = document.querySelector('#_marketing-dev-root');
-	if (devRoot) {
-		mount(devRoot);
-	}
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
 }
 
-// we are running through container and we should export the mount function
+// We are running through container
+// and we should export the mount function
 export { mount };
