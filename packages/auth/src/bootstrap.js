cd884a01e95e6a5ee0aedb0d4f8fb47884dc0a8c
salix <sal.senturk@gmail.com>
2021-05-10 22:09:02 +0300

    auth implementation

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -5,11 +5,12 @@ import App from './App';
 
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
-	const history = defaultHistory
-		? defaultHistory
-		: createMemoryHistory({
-				initialEntries: [initialPath],
-		  });
+	const history =
+		defaultHistory ||
+		createMemoryHistory({
+			initialEntries: [initialPath],
+		});
+
 	if (onNavigate) {
 		history.listen(onNavigate);
 	}
@@ -17,9 +18,11 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 	ReactDOM.render(<App history={history} />, el);
 
 	return {
-		onParentNavigate: ({ pathname }) => {
-			if (history.location.pathname !== pathname) {
-				history.push(pathname);
+		onParentNavigate({ pathname: nextPathname }) {
+			const { pathname } = history.location;
+
+			if (pathname !== nextPathname) {
+				history.push(nextPathname);
 			}
 		},
 	};
@@ -28,7 +31,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-	const devRoot = document.querySelector('#_marketing-dev-root');
+	const devRoot = document.querySelector('#_auth-dev-root');
 
 	if (devRoot) {
 		mount(devRoot, { defaultHistory: createBrowserHistory() });
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import MarketingApp from './components/MarketingApp';
+
 import Header from './components/Header';
+import Progress from './components/Progress';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
 	productionPrefix: 'co',
@@ -14,7 +19,12 @@ export default () => {
 			<StylesProvider generateClassName={generateClassName}>
 				<div>
 					<Header />
-					<MarketingApp />
+					<Suspense fallback={<Progress />}>
+						<Switch>
+							<Route path='/auth' component={AuthLazy} />
+							<Route path='/' component={MarketingLazy} />
+						</Switch>
+					</Suspense>
 				</div>
 			</StylesProvider>
 		</BrowserRouter>
